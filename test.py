d751fc8e04514ab6f3a4fb57f27fad3a91e20bce
qre <zaikingleb@gmail.com>
2022-07-22 12:16:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,14 +7,27 @@ data = [{"likes": 78, "name": "Agjd", "views": 10},
         {"likes": 69, "name": "Gleb", "views": 43934}, 
         {"likes": 999, "name": "Jim", "views": 1234}]
 
+s = requests.session()
+s.headers = headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+# https = s.put("https://reqbin.com/echo/post/json", json={"foo": "bar"})
+# print(https.status_code)
+# print(https.request.headers)
+
+# http = s.get("http://reqbin.com/echo/post/json", json={"foo": "bar"})
+# print(http.status_code)
+# print(http.request.headers)
+
 for i in range(len(data)):
 
-    response = requests.put(BASE + "video/" + str(i), data[i])
+    response = s.put(BASE + "video/" + str(i), data[i], json={"foo": "bar"})
     print(response.json())
 
 input()
-response = requests.delete(BASE + "video/0")
+s = requests.delete(BASE + "video/0", json={"foo": "bar"})
 print(response)
 input()
-response = requests.get(BASE + "video/2")
+s = requests.get(BASE + "video/2", json={"foo": "bar"})
 print(response.json())
